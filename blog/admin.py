2e67d861d8b185e9ae855e23e56cbc50383d8cf7
elnazpoyraz <elnaz.gharnas@gmail.com>
2024-01-26 23:24:44 +0200

    second commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Blog, Comment, PostView, Like
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Blog)
+admin.site.register(Comment)
+admin.site.register(PostView)
+admin.site.register(Like)
